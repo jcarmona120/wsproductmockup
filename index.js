f01d112753c4fe8a6cf105455ab661d039bf8a64
Jawann Carmona <carmona.jawann@gmail.com>
2018-07-29 22:16:18 -0400

    finished html/css design. next step to install accessbility markup and move to react

---
+++ index.js
@@ -0,0 +1,9 @@
+var expand = document.getElementsByClassName('product-summary')
+var drawer = document.getElementById('info-drawer')
+var arrow = document.getElementById('arrow-right')
+
+for (i = 0; i < expand.length; i++) {
+        console.log(expand[i])
+}
+
+
