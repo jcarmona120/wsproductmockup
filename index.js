ab7224b1847a3d1f38a8063c0242bbda0176bfc5
Jawann Carmona <carmona.jawann@gmail.com>
2018-08-05 23:54:00 -0400

    port change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
-const port = 3000 || process.env.port
+const port = process.env.port || 3000
 
 app.use(express.static(__dirname + '/public' ));
 
